c47f2132c0d1381337e35618262edfc9b44389ce
wowdyaln <rogersmk@gmail.com>
2022-04-04 11:18:11 +0800

    class Cell

--- Grid.js
+++ Grid.js
@@ -1,5 +1,4 @@
 /*
-
 --grid-size: 4;
 --cell-size: 20vmin;
 --cell-gap: 2vmin;
@@ -9,29 +8,41 @@ const GRID_SIZE = 4
 const CELL_SIZE = 20
 const CELL_GAP = 2
 
-
 export default class Grid {
   constructor(gridElement) {
     gridElement.style.setProperty("--grid-size", GRID_SIZE)
     gridElement.style.setProperty("--cell-size", `${CELL_SIZE}vmin`)
     gridElement.style.setProperty("--cell-gap", `${CELL_GAP}vmin`)
+
     // create 12 cell elements
-    
-    createCellElements(gridElement)
-    
+    this.cells = createCellElements(gridElement).map(
+      (cellEle, index) => {
+        return new Cell(
+          cellEle,
+          index % GRID_SIZE,
+          Math.floor(index / GRID_SIZE)
+        )
+      }
+    )
   }
 }
 
+//* 每個 cell 有自己的資料
+class Cell {
+  constructor(cellEle, x, y) {
+    this.cellEle = cellEle
+    this.x = x
+    this.y = y
+  }
+}
 
-
-
-const createCellElements= (gridEle) => {
+const createCellElements = (gridEle) => {
   const cells = []
-  for (let i = 0; i < GRID_SIZE * GRID_SIZE ; i++) {
-    const cell = document.createElement('div')
-    cell.classList.add('cell')
+  for (let i = 0; i < GRID_SIZE * GRID_SIZE; i++) {
+    const cell = document.createElement("div")
+    cell.classList.add("cell")
     cells.push(cell)
-    gridEle.append(cell)
+    gridEle.append(cell) //* DOM div#game-board
   }
   return cells
 }
