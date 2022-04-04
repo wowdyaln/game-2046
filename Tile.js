5644ad6443c2d287d8cf487f91c21d0ac37005f3
wowdyaln <rogersmk@gmail.com>
2022-04-04 14:26:18 +0800

    set tile css style

--- Tile.js
+++ Tile.js
@@ -15,6 +15,18 @@ export default class Tile {
   set value(v) {
     this.#value = v
     this.#tileElement.textContent = v
+
+    const power = Math.log2(v)
+    // const power = 9
+    const backgroundLightness = 100 - power * 9
+    this.#tileElement.style.setProperty(
+      "--background-lightness",
+      `${backgroundLightness}%`
+    )
+    this.#tileElement.style.setProperty(
+      "--text-lightness",
+      `${backgroundLightness <= 50 ? 90 : 10}%`
+    )
   }
 
   set x(value) {
