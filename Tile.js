de2960b8f9862049b28a1d98934fd6a5f30556ed
wowdyaln <rogersmk@gmail.com>
2022-04-04 14:11:18 +0800

    create two random tiles

--- Tile.js
+++ Tile.js
@@ -1,15 +1,23 @@
 export default class Tile {
   #tileElement
+  #x
+  #y
 
-  // tileContainer
+  // tileContainer, 就是 gameBoard
   constructor(tileContainer, value = Math.random() > 0.5 ? 2 : 4) {
     this.#tileElement = document.createElement("div")
     this.#tileElement.classList.add("tile")
     tileContainer.append(this.#tileElement)
-    this.#value = value
+    this.value = value
   }
 
-  set x(value) {}
+  set x(value) {
+    this.#x = value
+    this.#tileElement.style.setProperty("--x", value)
+  }
 
-  set y(value) {}
+  set y(value) {
+    this.#y = value
+    this.#tileElement.style.setProperty("--y", value)
+  }
 }
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 import Grid from "./Grid.js"
+import Tile from "./Tile.js"
 
 const gameBoard = document.getElementById("game-board")
 
@@ -7,5 +8,5 @@ const grid = new Grid(gameBoard)
 
 console.log(grid.randomEmptyCell())
 
-// grid.randomEmptyCell().tile = new Tile(gameBoard)
-// grid.randomEmptyCell().tile = new Tile(gameBoard)
+grid.randomEmptyCell().tile = new Tile(gameBoard)
+grid.randomEmptyCell().tile = new Tile(gameBoard)
